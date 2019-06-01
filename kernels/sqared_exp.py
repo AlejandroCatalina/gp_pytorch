054278396701291978296f1ea6a3f01b5f98a479
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2019-06-01 23:30:59 +0300

    add todo to change kernel parameters to be only positive

--- kernels/sqared_exp.py
+++ kernels/sqared_exp.py
@@ -6,6 +6,7 @@ import pdb
 class SquaredExp(nn.Module):
     def __init__(self):
         super(SquaredExp, self).__init__()
+        # TODO: positive arguments
         self.lengthscale = nn.Parameter(torch.nn.init.normal_(torch.empty(1,1)))
         self.prefactor = nn.Parameter(torch.nn.init.normal_(torch.empty(1,1)))
         # print(f"lengthscale = {self.lengthscale.shape}")
@@ -16,9 +17,9 @@ class SquaredExp(nn.Module):
         # print(f"l2 {l2.shape}")
         pref_sq = self.prefactor**2
         ls_sq   = self.lengthscale**2
-        # print(f"pref_sq {pref_sq.shape}") 
-        # print(f"ls_sq {ls_sq.shape}") 
+        # print(f"pref_sq {pref_sq.shape}")
+        # print(f"ls_sq {ls_sq.shape}")
         # so that the following operations can be broadcast.
-        l2      = l2.transpose(1, -1) 
+        l2      = l2.transpose(1, -1)
         kernel  = pref_sq * torch.exp(-0.5 * l2 / ls_sq)
         return kernel.squeeze()
