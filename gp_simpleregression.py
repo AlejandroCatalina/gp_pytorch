595735d55c8caa7c64b1088e334d1c9134c4846f
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2019-06-11 23:56:28 +0300

    refactor so it matches new definition

--- gp_simpleregression.py
+++ gp_simpleregression.py
@@ -25,16 +25,16 @@ y_noisy = y + np.random.normal(0, consts.noisestd) # noisy target
 x_test   = torch.linspace(-10, 10, consts.Ntest).reshape((-1, 1, 1)) # test data
 kernel  = SquaredExp() # kernel
 
-model = GPR(kernel)
+model = GPR(x, y, kernel)
 
-def train(module, X, y, n_iters=50):
+def train(module, n_iters=50):
     # optimize log marginal likelihood
     opt = torch.optim.Adam(module.parameters(), lr=1e-3)
 
     # training loop
     for iter in range(n_iters):
         opt.zero_grad()
-        nmll = module(X, y)
+        nmll = module()
         nmll.backward()
         opt.step()
         print(f"Iter {iter} , Log marginal likelihood : {-nmll.item()} ")
@@ -42,7 +42,7 @@ def train(module, X, y, n_iters=50):
         print(f"Kernel prefactor {model.kernel.prefactor.item()}")
         print(f"Noise std {model.noise_std.item()}")
 
-train(model, x, y)
+train(model)
 posterior_mean, posterior_var = model.predict(x_test)
 
 visualize(x, y, y_noisy, x_test, posterior_mean, posterior_var) # x , true function, noisy function, x_test, prediction_mean, pred_var, filename
