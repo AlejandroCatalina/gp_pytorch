f663e9e3c1398f6584444ee9c84293a5dd150946
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2019-06-11 16:13:14 +0300

    add predict

--- models/GPR.py
+++ models/GPR.py
@@ -4,6 +4,7 @@ import pdb
 import numpy as np
 import torch
 import torch.nn as nn
+from torch import distributions as dist
 from torch.nn.init import uniform_
 
 
@@ -27,5 +28,17 @@ class GPR(nn.Module):
             - torch.sum(torch.diag(L.squeeze())) \
             - 0.5*n*torch.log(2*torch.tensor(math.pi))
 
-    def predict(self, x):
-        raise NotImplementedError
+    def predict(self, x_test, x, y, full_cov = True):
+        N, M = x.shape
+        Ntest, _ = x_test.shape
+        Kxx = self.kernel(x, x) + 1e-3 * torch.eye(N)
+        Kxx_inv = (Kxx + self.noise_std * torch.eye(N)).inverse()
+        Ks = self.kernel(x, x_test)
+        Kss = self.kernel(x_test, x_test)
+
+        m_pred = Ks.t() @ Kxx_inv @ y
+        k_pred = (Kss + self.noise_std * torch.eye(N_test)
+                  - Ks.t() @ Kxx_inv @ Ks)
+        if not full_cov:
+            k_pred = k_pred.diag().sqrt()
+        return m_pred, k_pred
