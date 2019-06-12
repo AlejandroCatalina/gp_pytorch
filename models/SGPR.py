8cdb463ef57daa6f5b9419baa33d72178bd72d38
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2019-06-12 11:27:48 +0300

    change ELBO to include q(u) and optimize over it

--- models/SGPR.py
+++ models/SGPR.py
@@ -12,6 +12,8 @@ class SGPR(GPR):
         self.M = M
         self.N, D = X.shape
         self.Z = nn.Parameter(normal_(torch.empty(M, D)))
+        self.m = nn.Parameter(normal_(torch.empty(M, D)))
+        self.S = nn.Parameter(torch.exp(uniform_(torch.empty(1), -3, 0)) * torch.eye(M))
 
     def forward(self):
         N, M = self.N, self.M
@@ -23,15 +25,34 @@ class SGPR(GPR):
         elbo = -self.elbo(Knn, Knm, Kmm, Qnn, y)
         return elbo
 
+    def kl(self, m_q, S_q, m_p, S_p):
+        M = self.M
+        log_ratio_det = torch.log(S_p.det() / S_q.det()) - M
+        trace = torch.trace(S_p.inverse() @ S_q)
+        mu = (m_p - m_q).t() @ S_p.inverse() @ (m_p - m_q)
+        return 0.5 * (log_ratio_det + trace + mu)
+
     def elbo(self, Knn, Knm, Kmm, Qnn, y):
+        """Implement variational ELBO as in
+        Hensman et al, Scalable Variational Gaussian Process Classification, ICML 2015.
+        """
         N = self.N
-        K = Knm @ Kmm.inverse() @ Knm.t() + 1e-1 * torch.eye(N)
-        L = torch.cholesky(K + self.noise_std**2 * torch.eye(N))
-        a, _      = torch.solve(y, L.transpose(-1, 0))
-        alpha, _  = torch.solve(a, L.transpose(-1, 0))
-        return - 0.5 * y.t() @ alpha \
+        Kmm_inv = Kmm.inverse()
+        Knm_Kmm_inv = Knm @ Kmm_inv
+        m = Knm_Kmm_inv @ self.m
+        S = self.noise_std**2 * torch.eye(N)
+
+        ## compute KL[q_u || p_u], with p_u = N(0, I) and q_u = N(m, S)
+        kl_q_u_p_u = self.kl(self.m, self.S, torch.zeros_like(self.m), torch.eye(self.M))
+
+        ## return elbo
+        ## TODO: rewrite with cholesky decomposition
+        return - 0.5 * (y - m).t() @ S.inverse() @ (y - m) \
+            - 1. / (2 * self.noise_std**2) \
+            * torch.sum(torch.diag(Knm_Kmm_inv @ self.S @ Kmm_inv @ Knm.t())) \
             - 1. / (2 * self.noise_std**2) * torch.sum(torch.diag(Knn - Qnn)) \
-            - 0.5 * N * torch.log(2 * torch.tensor(math.pi))
+            - 0.5 * N * torch.log(2 * torch.tensor(math.pi)) \
+            - kl_q_u_p_u
 
     def predict(self, x_test):
         raise NotImplementedError
