b1703c66ff408070bac729e76d4e68e48dac013b
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2019-06-13 15:33:23 +0300

    small-big refactor
    still need to introduce D_out

--- models/GPR.py
+++ models/GPR.py
@@ -8,48 +8,46 @@ from torch.nn.init import uniform_, normal_
 
 
 class GPR(nn.Module):
-    def __init__(self, X, y, kernel):
+    def __init__(self, kernel):
         super(GPR, self).__init__()
         self.kernel = kernel
-        N = X.shape[0]
-        self.X, self.y = X.reshape(N, -1), y.reshape(-1, 1)
         self.noise_std = nn.Parameter(torch.exp(uniform_(torch.empty(1, 1), -3., 0.)))
 
-    def forward(self):
-        x, y = self.X, self.y
-        Kxx = self.kernel(x, x) + 1e-3 * torch.eye(x.shape[0])
-        nmll = -self.log_marginal_likelihood(Kxx, y)
-        return nmll
+    def forward(self, X, y):
+        N, _ = X.shape
 
-    def log_marginal_likelihood(self, K, y):
-        n = K.shape[0]
-        L = torch.cholesky(K + self.noise_std**2 * torch.eye(n))
-        a, _     = torch.solve(y.unsqueeze(0), L.unsqueeze(0))
-        alpha, _ = torch.solve(a, L.unsqueeze(0))
-        alpha = alpha.squeeze(0)
-        return -0.5 * y.t() @ alpha \
-            - torch.sum(torch.diag(L)) \
-            - 0.5*n*torch.log(2*torch.tensor(math.pi))
-
-    def predict(self, x_test, full_cov = True):
-        x, y = self.X, self.y
-        N, D = x.shape
-        N_test = x_test.shape[0]
-
-        if len(x_test.shape) > 2:
-            x_test = x_test.reshape(N_test, -1)
-
-        N, M      = x.shape
-        N_test, _ = x_test.shape
-
-        Kxx = self.kernel(x, x) + 1e-3 * torch.eye(N)
+        Kxx = self.kernel(X, X) + 1e-3 * torch.eye(N)
         Kxx_inv = (Kxx + self.noise_std * torch.eye(N)).inverse()
-        Ks = self.kernel(x, x_test)
-        Kss = self.kernel(x_test, x_test)
 
-        m_pred = Ks.t() @ Kxx_inv @ y
-        k_pred = (Kss + self.noise_std * torch.eye(N_test) \
-                  - Ks.t() @ Kxx_inv @ Ks)
+        self.Kxx_inv = Kxx_inv
+        self.X = X
+
+        mu = Kxx.t() @ Kxx_inv @ y
+        cov = (Kxx + self.noise_std * torch.eye(N)
+               - Kxx.t() @ Kxx_inv @ Kxx)
+        return mu, cov
+
+    def neg_log_lik(self, X, y):
+        mu, cov = self.forward(X, y)
+        S = self.noise_std ** 2 * torch.eye(self.N)
+
+        return (- 0.5 * (y - mu).t() @ S.inverse() @ (y - mu)
+                - 1. / (2 * self.noise_std ** 2) * torch.trace(cov)
+                - 0.5 * N * torch.log(2 * torch.tensor(math.pi)))
+
+    def KL(self):
+        return 0.
+
+    def predict(self, X_test, full_cov = True):
+        N = X_test.shape[0]
+        X, Kxx_inv = self.X, self.Kxx_inv
+
+        Ks = self.kernel(X_test, X) + 1e-3 * torch.eye(N)
+        Kss = self.kernel(X_test, X_test) + 1e-3 * torch.eye(N)
+
+        mu = Ks.t() @ Kxx_inv @ y
+        cov = (Ks + self.noise_std * torch.eye(N)
+               - Ks.t() @ Kxx_inv @ Ks)
         if not full_cov:
-            k_pred = k_pred.diag().sqrt()
-        return m_pred, k_pred
+            return mu, cov.diag().sqrt()
+        return mu, cov
