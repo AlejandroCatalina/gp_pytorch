e8a0b61eb629ff248eefb568ed0afc6e83636b3b
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2019-06-14 14:20:52 +0300

    updated rbf kernel
    use log_sigma and log_alpha

--- kernels/sqared_exp.py
+++ kernels/sqared_exp.py
@@ -11,12 +11,18 @@ uniform_ = torch.nn.init.uniform_
 class SquaredExp(nn.Module):
     def __init__(self):
         super(SquaredExp, self).__init__()
-        self.lengthscale = nn.Parameter(torch.exp(uniform_(torch.empty(1, 1), -3, 0)))
-        self.prefactor = nn.Parameter(torch.exp(uniform_(torch.empty(1, 1), -1, 1)))
+        self.lengthscale = nn.Parameter(uniform_(torch.empty(1, 1), -3, 0))
+        self.prefactor = nn.Parameter(uniform_(torch.empty(1, 1), -1, 1))
 
-    def forward(self, X1, X2):
-        l2      = (X1 - X2.transpose(1, 2))**2
-        pref_sq = self.prefactor**2
-        ls_sq   = self.lengthscale**2
-        kernel  = pref_sq * torch.exp(-0.5 * l2 / ls_sq)
-        return kernel
+    def forward(self, X, Z):
+        gamma = self.lengthscale
+        alpha = self.prefactor
+        if Z is None:
+            Z = X
+        scaled_X = X / gamma ** 2
+        scaled_Z = Z / gamma ** 2
+        X2 = (scaled_X ** 2).sum(-1, keepdim = True)
+        Z2 = (scaled_Z ** 2).sum(-1, keepdim = True)
+        XZ = scaled_X.matmul(scaled_Z.transpose(1, 2))
+        r2 = (X2 - 2 * XZ + Z2.transpose(1, 2)).clamp(min = 0)
+        return torch.exp(alpha) ** 2 * torch.exp(gamma) ** 2 * torch.exp(-0.5 * r2)
