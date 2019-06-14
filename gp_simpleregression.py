793040b2e11381cbf758393db6e7bd52c0e07b62
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2019-06-14 17:43:02 +0300

    add pyro example's data

--- gp_simpleregression.py
+++ gp_simpleregression.py
@@ -34,21 +34,30 @@ x_test_ = (x_test - x.mean()) / x.std()
 kernel  = SquaredExp() # kernel
 model = SGPR(D = 1, M = 20, kernel = kernel, Z = z)
 
-def train(module, x, y, n_iters=50, lr = 1e-3):
+def train(module, x, y_noisy, y = None, x_test = None, n_iters=50, lr = 1e-3, plot = False):
     # optimize log marginal likelihood
     opt = torch.optim.Adam(module.parameters(), lr=lr)
 
     # training loop
     for iter in range(n_iters):
         opt.zero_grad()
-        nmll = -elbo(model, x, y)
+        nmll = -elbo(model, x, y_noisy)
         nmll.backward()
         opt.step()
         print(f"Iter {iter} , Log marginal likelihood : {-nmll.item()} ")
-        # print(f"Kernel lengthscale {module.kernel.lengthscale.item()}")
-        # print(f"Kernel prefactor {module.kernel.prefactor.item()}")
-        # print(f"Noise std {module.noise_std.item()}")
+        if plot and y is not None and x_test is not None and not iter % 50:
+            posterior_mean, posterior_var = model.predict(x_test, full_cov=False)
+            visualize(x, y, y_noisy, x_test, posterior_mean, posterior_var, f"{model}-{iter}.pdf")
 
 train(model, x, y, n_iters = 2500, lr = 1e-3)
-posterior_mean, posterior_var = model.predict(x_test, full_cov=False)
+posterior_mean, posterior_var = model.predict(x_test_, full_cov=False)
 visualize(x, y, y_noisy, x_test, posterior_mean, posterior_var, "GP-sparse.pdf") # x , true function, noisy function, x_test, prediction_mean, pred_var, filename
+
+#
+N = 1000
+X = torch.linspace(0.0, 5.0, N)
+y = 0.5 * torch.sin(3*X)
+y_noisy = y + dist.Normal(0.0, 0.2).sample(sample_shape=(N,))
+X_ = (X - X.mean()) / X.std()
+X_test = X_
+
